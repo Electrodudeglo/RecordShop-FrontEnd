f530babb06f25699aa3be2e04f8145deb59e2f8b
Mohamed <mohamedmohamed1812@gmail.com>
2026-05-19 13:00:37 +0100

    UI: Testing different UI's for the front-end

---
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/Models/MusicRecordModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RecordShop_FrontEnd.Models
+{
+    public class MusicRecordModel
+    {
+        public int Id { get; set; }
+        [JsonPropertyName("record_title")]
+        public string RecordTitle { get; set; }
+        public string Artists { get; set; }
+        [JsonPropertyName("release_year")]
+        public string ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public int Stock { get; set; }
+    }
+
+
+
+}
