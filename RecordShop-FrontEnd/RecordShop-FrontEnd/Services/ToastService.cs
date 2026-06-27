eec15de1e5384c1a9ab079bd256096d6dcaa533d
Mohamed Lenovo Laptop <mohamedmohamed1812@gmail.com>
2026-06-27 17:31:00 +0100

    Add: Add interface folder Add: Files IToastService,ToastEnum, ToastService

---
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/Services/ToastService.cs
@@ -0,0 +1,38 @@
+﻿using RecordShop_FrontEnd.Components.UI;
+using RecordShop_FrontEnd.Interfaces;
+
+namespace RecordShop_FrontEnd.Services
+{
+    public class ToastService : IToastService
+{
+        private readonly List<ToastMessageClass> _messages = new();
+        public event Action? OnChange;
+        public IReadOnlyList<ToastMessageClass> Messages => _messages;
+
+        public void Show(string text, ToastEnum type = ToastEnum.Info, TimeSpan? duration = null)
+        {
+            var toast = new ToastMessageClass
+            {
+                Text = text,
+                Type = type,
+                Duration = duration ?? TimeSpan.FromSeconds(3)
+            };
+
+            _messages.Add(toast);
+            OnChange?.Invoke();
+
+            _ = AutoRemoveAsync(toast);
+        }
+        public void Remove(ToastMessageClass message)
+        {
+            if (_messages.Remove(message)) OnChange?.Invoke();
+        }
+
+        private async Task AutoRemoveAsync(ToastMessageClass toast)
+        {
+            await Task.Delay(toast.Duration);
+            Remove(toast);
+        }
+
+    }
+}
---
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/ToastEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordShop_FrontEnd
+{
+    public enum ToastEnum
+    {
+        Success,
+        Error,
+        Warning,
+        Info
+    }
+}
