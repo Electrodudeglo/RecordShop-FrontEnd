e970540cf6815cddafab098b6f526c0697c5475f
Mohamed Lenovo Laptop <mohamedmohamed1812@gmail.com>
2026-06-27 16:27:28 +0100

    Add: Add toast for login and logout message

--- RecordShop-FrontEnd/RecordShop-FrontEnd/Models/MusicRecordModel.cs
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/Models/MusicRecordModel.cs
@@ -6,13 +6,13 @@ namespace RecordShop_FrontEnd.Models
     {
         public int Id { get; set; }
         [JsonPropertyName("record_title")]
-        public string RecordTitle { get; set; }
-        public string Artists { get; set; }
+        public string RecordTitle { get; set; } = String.Empty;
+        public string Artists { get; set; } = String.Empty;
         [JsonPropertyName("release_year")]
-        public string ReleaseYear { get; set; }
-        public string Genre { get; set; }
+        public string ReleaseYear { get; set; } = String.Empty;
+        public string Genre { get; set; } = String.Empty;
         public int Stock { get; set; }
-        public string Cover { get; set; }
+        public string Cover { get; set; } = String.Empty;
 
     }
 
