ee6c4cd9e7af79c0f7c1720187bc7936faab163f
Mohamed <mohamedmohamed1812@gmail.com>
2026-06-28 18:21:14 +0100

    Feat: Wired the delete pipeline to backend

--- RecordShop-FrontEnd/RecordShop-FrontEnd/Services/RecordService.cs
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/Services/RecordService.cs
@@ -1,18 +1,21 @@
 ﻿namespace RecordShop_FrontEnd.Services
 {
+    using RecordShop_FrontEnd.Interfaces;
+    using RecordShop_FrontEnd.Models;
     using System.Net.Http.Headers;
     using System.Net.Http.Json;
-    using RecordShop_FrontEnd.Models;
     public class RecordService
 {
 
         private readonly HttpClient _http;
         private readonly AuthService _auth;
+        private readonly IToastService _toast;
 
-        public RecordService(HttpClient http, AuthService auth)
+        public RecordService(HttpClient http, AuthService auth, IToastService toastService)
         {
             _http = http;
             _auth = auth;
+            _toast = toastService;
         }
 
 
@@ -48,8 +51,14 @@
 
         public async Task<bool> DeleteRecord(int id)
         {
-            if (!await AttachToken()) return false;
-            var response = await _http.DeleteAsync($"api/musicrecord{id}");
+            if (!await AttachToken()) { _toast.Show("Unauthorized",ToastEnum.Error); return false; }
+            var response = await _http.DeleteAsync($"api/v1/records/{id}");
+            
+            if(response.IsSuccessStatusCode)
+            {
+                _toast.Show("Successfully Deleted", ToastEnum.Success);
+            }
+
             return response.IsSuccessStatusCode;
         }       
 }
