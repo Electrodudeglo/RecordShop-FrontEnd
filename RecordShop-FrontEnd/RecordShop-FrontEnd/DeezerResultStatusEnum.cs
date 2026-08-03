cc0a988c0323d4b193d4850978d622768a6d9e05
Mohamed Abdullahi <Mohamed_Victorydance@hotmail.com>
2026-08-04 00:51:09 +0100

    Add: Add another enum for deezer api error codes

---
+++ RecordShop-FrontEnd/RecordShop-FrontEnd/DeezerResultStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordShop_FrontEnd
+{
+    public enum DeezerResultStatusEnum
+    {
+        Success,
+        NotFound,
+        ServerError,
+        NetworkError,
+        InvalidJson,
+        DeezerErrorPayload
+    }
+
+}
